ab53f68c0778056ad1d432decd8cffc75f77174b
cisimon7 <cisimon7@gmail.com>
2021-11-13 04:16:59 +0100

    Lab 1 Task 2

--- Lab 1/Cart_Pole.py
+++ Lab 1/Cart_Pole.py
@@ -1,4 +1,3 @@
-import os
 import time
 import gym
 
@@ -28,5 +27,37 @@ def cart_pole():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    os.environ['LANG'] = 'en_US'
     cart_pole()
+
+
+"""
+    Observation Vector:
+        An array of size 4, containing:
+        Idx     Observation               Min                     Max
+        0       Cart Position             -4.8                    4.8
+        1       Cart Velocity             -Inf                    Inf
+        2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
+        3       Pole Angular Velocity     -Inf                    Inf
+        
+        
+    Action Vector:
+        A set of two distinct discrete numbers
+        Num   Action
+        0     Push cart to the left
+        1     Push cart to the right
+        
+        
+    How reward is calculated:
+        The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the 
+        cart's velocity. For each step taken, that is the robot is not fallen, the reward is a constant: 1
+    
+    
+    How initial conditions are varied by the reset method:
+        An array of size 4 is generated using a uniform random distribution bounded between -0.05 amd 0.05
+        
+        
+    What are the termination conditions:
+        Fallen state, i.e. Angle and distance at which to fail the episode, is defined at initialization of system. 
+        At this angle or distance, the pendulum is taken as fallen. Environment terminates if observation vector 
+        violates any of this limit.
+"""
